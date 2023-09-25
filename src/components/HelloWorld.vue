f75d6c7bae63eba77445290c0281064e4c06b675
androllen <androllen@hotmail.com>
2023-09-26 02:59:33 +0800

    update

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,98 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <div style="width:100%; height:300px;" ref="myEcharts"></div>
+    <el-button @click="sendMessage">Send</el-button>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  data() {
+    return {
+      message: "",
+      receivedData: "",
+      option: {},
+      dataDiagramData: [5, 20, 36, 10, 10, 20],
+      chartBar: {},
+    };
+  },
+  mounted() {
+    this.connectWebSocket();
+    this.initChartsDiagram();
+  },
+  created() {
+    this.option = {};
+  },
+  methods: {
+    connectWebSocket() {
+      const webSocketURL = "ws://localhost:8000/ws/chat";
+      const webSocket = new WebSocket(webSocketURL);
+      webSocket.onmessage = this.handleSocketMessage;
+    },
+    handleSocketMessage(event) {
+      const data = JSON.parse(event.data);
+      this.dataDiagramData = data.message;
+      this.initChartsDiagram();
+    },
+    sendMessage() {
+      // const webSocket = this.getWebSocket();
+      // const data = JSON.stringify({ message: this.message });
+      // webSocket.send(data);
+      axios
+        .get("http://127.0.0.1:8000/api/get/")
+        .then(function (response) {
+          // 处理返回的数据
+          this.$message("这是一条消息提示");
+          if (response.message === "ws send ok") {
+          }
+        })
+        .catch(function (error) {
+          // 处理错误
+          console.log(error);
+        });
+    },
+    initChartsDiagram() {
+      this.chartBar = this.$echarts.init(this.$refs.myEcharts);
+
+      this.option = {
+        title: {
+          text: "Echarts 入门示例",
+        },
+        tooltip: {},
+        xAxis: {
+          data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"],
+        },
+        yAxis: {},
+        series: [
+          {
+            name: "销量",
+            type: "bar",
+            data: this.dataDiagramData,
+            smooth: true,
+          },
+        ],
+      };
+      this.chartBar.setOption(this.option);
+    },
+    getWebSocket() {
+      const webSocketURL = "ws://localhost:8000/ws/";
+      return new WebSocket(webSocketURL);
+    },
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.container {
+  width: 300px;
+  height: 300px;
+  margin-left: 30px;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+#echart {
+  width: 100%;
+  height: 100%;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import echarts from 'echarts'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 
+Vue.use(ElementUI);
+Vue.prototype.$echarts = echarts //挂载到Vue实例上面
 Vue.config.productionTip = false
 
 new Vue({
